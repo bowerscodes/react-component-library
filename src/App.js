cf2ceb370cd238d3ff74d4083ec1a7240a4b1d80
James Bowers <jamesbowers123@gmail.com>
2023-02-18 14:45:49 +0000

    Structure Storybook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Button from "./components/Button/Button"
 import TextInput from "./components/TextInput/TextInput";
 
 import ".././public/stylesheets/styles.css";
 
 export function App() {
+
+  ButtonProps={
+    primary: true,
+    backgroundColor: 'rgba(255, 255, 255)',
+    size: 'medium',
+    label: 'Button',
+  }
 
   TextInputProps={
     id: "text-input",
@@ -13,6 +21,12 @@ export function App() {
   return (
     <div id="app">
       <h1>Component Library</h1>
+      <Button 
+        primary={ButtonProps.primary}
+        backgroundColor={ButtonProps.backgroundColor}
+        size={ButtonProps.size}
+        label={ButtonProps.label}
+      />
       <TextInput 
         props={TextInputProps}
       />
