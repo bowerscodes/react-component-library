725d9a4b2841967ee7819dafcb950cc981d5a01e
James Bowers <jamesbowers123@gmail.com>
2023-07-03 16:47:11 +0100

    add warningPage

--- src/stories/FormPage.stories.tsx
+++ src/stories/FormPage.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react';
 
 import { FormPage } from '../components/FormPage/FormPage';
 import { page } from '../pages/ExamplePage1.json';
+import { page as warningPage } from '../pages/WarningPage.json';
 
 const meta: Meta<typeof FormPage> = {
   title: 'FormPage',
@@ -15,5 +16,11 @@ type Story = StoryObj<typeof FormPage>;
 export const Default: Story = {
   args: {
     page: page
+  }
+};
+
+export const Warning: Story = {
+  args: {
+    page: warningPage
   }
 };
