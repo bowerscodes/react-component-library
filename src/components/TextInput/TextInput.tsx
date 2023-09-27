25dc4f786ef822c2028b2a2b36edf2377e52f4f2
James Bowers <jamesbowers123@gmail.com>
2023-09-27 13:39:19 +0100

    refactor TetInput and test

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-
 import { Label } from '../Label/Label';
 import { classBuilder, cleanHtmlAttributes, COMPONENT_TYPES, toArray } from '../../utils/Utils';
 import './TextInput.scss';
 
 interface TextInputProps {
   id: string;
-  fieldId: string;
+  fieldId?: string;
   disabled?: boolean;
   error?: any;
   readonly?: boolean;
   textInputLabel?: string;
   placeholder?: string;
   value?: string;
-  onChange: () => void;
+  onChange?: () => void;
   width?: 's' | 'm' | 'l' | 'xl' | 'one-half' | 'one-third' | 'two-thirds';
   classBlock?: any;
   classModifiers?: any;
@@ -25,7 +23,7 @@ export const DEFAULT_CLASS = 'textInput'
 
 export const TextInput = ({
   id,
-  fieldId= id,
+  fieldId = id,
   disabled = false,
   error = false,
   readonly = false,
@@ -34,39 +32,34 @@ export const TextInput = ({
   onChange,
   width = 'm',
   classBlock ='textInput',
-  classModifiers: _classModifiers = width,
+  classModifiers: _classModifiers = [width],
   className = '',
   ...attrs
 }: TextInputProps) => {
-
-  console.log('_classModifiers: ',_classModifiers)
-  console.log('className: ',className)
-  console.log('classBlock: ',classBlock)
   
   const classModifiers = [...toArray(_classModifiers), error ? 'error' : undefined];
   const classes = classBuilder(classBlock, classModifiers, className)
 
   const cleanedAttrs = cleanHtmlAttributes(attrs, COMPONENT_TYPES.INPUT)
 
+  const label = <Label children={textInputLabel} />
+  
   const input = (
-    <input
-      {...cleanedAttrs}
-      disabled={disabled}
-      id={id}
-      name={fieldId}
-      type="text"
-      onChange={onChange}
-      className={classes()}
-    />
+    <div className="input-container">
+      {textInputLabel && label}
+      <input
+        {...cleanedAttrs}
+        disabled={disabled}
+        id={id}
+        name={fieldId}
+        type="text"
+        onChange={onChange}
+        className={classes()}
+      />
+    </div>
   );
 
-  return (
-    <div className='input-container'>
-      <Label
-        children = {textInputLabel}
-        size = 'm'
-      />
-      {input}
-    </div>
-  )
+  return input;
 };
+
+export default TextInput;
