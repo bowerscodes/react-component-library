903fe9fcdcf5ac2baf45167deb61389c76eca425
James Bowers <jamesbowers123@gmail.com>
2023-09-27 09:14:29 +0100

    add classBuilder to Hint and test

--- src/components/Hint/Hint.tsx
+++ src/components/Hint/Hint.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
-
+import { classBuilder, toArray } from '../../utils/Utils';
 import './Hint.scss';
 
 interface HintProps {
   hintText?: string;
   size?: 's' | 'm' | 'l';
+  classBlock?: string;
+  classModifiers?: string[];
+  className?: string;
 };
+
+export const DEFAULT_CLASS = 'hint';
 
 export const Hint = ({
   hintText,
   size = 'm',
+  classBlock = DEFAULT_CLASS,
+  classModifiers: _classModifiers = [size],
+  className = '',
   ...props
+  
 }: HintProps) => {
+
+  const classModifiers = [...toArray(size)];
+  const classes = classBuilder(classBlock, classModifiers, className)
+
+
   return (
-    <div className={['hint', `hint--${size}`].join(' ')}>
+    <div className={classes()}>
       {hintText}
     </div>
-  )
-}
+  );
+};
+
+export default Hint;
