aa2527ba041489d3adc14bb074945d2dba12d13a
James Bowers <jamesbowers123@gmail.com>
2023-09-27 09:04:09 +0100

    add tests for Heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-
+import { classBuilder, toArray } from '../../utils/Utils';
 import './Heading.scss';
 
 type Props = {
   id?: string;
   headingText: string;
   size?: "s" | "m" | "l" | "xl";
+  classBlock?: string;
+  classModifiers?: string[];
+  className?: string;
 };
+
+export const DEFAULT_CLASS = 'heading';
 
 export const Heading = ({
   id,
   headingText,
   size = "m",
+  classBlock = DEFAULT_CLASS,
+  classModifiers: _classModifiers = [size],
+  className = '',
 }: Props) => {
+
+  const classModifiers = [...toArray(_classModifiers)];
+  const classes = classBuilder(classBlock, classModifiers, className)
+
   return (
     <div className="heading-container">
-      <h1 className={`heading heading--${size}`} id={id}>
+      <h1 className={classes()} id={id}>
         {headingText}
       </h1>
     </div>
   )
 };
+
+export default Heading;
