ae567eefe3688d3baac3ba4a6c1f84b02854404b
James Bowers <jamesbowers123@gmail.com>
2023-09-27 09:20:18 +0100

    add clasBuilder to Label and test

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
+
+import { classBuilder, toArray } from '../../utils/Utils';
 import './Label.scss';
 
 type Props = {
   key?: string;
   children: React.ReactNode;
   size?: 'xs' | 's' | 'm' | 'l' | 'xl';
+  classBlock?: string;
+  classModifiers?: string[];
+  className?: string;
 };
 
+export const DEFAULT_CLASS = 'label';
 
 export const Label = ({
   children,
   size = 'm',
+  classBlock = DEFAULT_CLASS,
+  classModifiers: _classModifiers = [size],
+  className = '',
 }: Props) => {
+
+  const classModifiers = [...toArray(_classModifiers)];
+  const classes = classBuilder(classBlock, classModifiers, className)
+
   return (
-    <label className={['label', `label--${size}`].join(' ')}>
+    <label className={classes()}>
       {children}
     </label>
   )
 };
+
+export default Label;
