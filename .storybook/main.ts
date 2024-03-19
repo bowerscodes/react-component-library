fd06683a3b758f4dfd4da95fdae70d2dc609fc1e
James Bowers <jamesbowers123@gmail.com>
2024-03-19 21:08:33 +0000

    remove unused addon storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,15 +11,6 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
-    // {
-    //   name: "@storybook/addon-docs",
-    //   options: {
-    //     configureJSX: true,
-    //     babelOptions: {},
-    //     sourceLoaderOptions: null,
-    //     transcludeMarkdown: true
-    //   }
-    // },
     "@storybook/addon-docs",
     "@storybook/addon-links",
     "@storybook/addon-essentials" ,
